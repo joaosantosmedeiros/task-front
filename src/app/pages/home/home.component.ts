bafadf0dea11137779d5e6085545c8ce72151866
joao pedro santos <jopesame@gmail.com>
2025-01-26 15:18:19 -0300

    add formulario no home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,28 +4,70 @@ import { Task } from '../../shared/models/Task';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatProgressSpinnerModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
+
   constructor(private taskService: TaskService) {}
 
   tasks: Task[] = [];
   isLoadingTasks: boolean = true;
 
+  submitted = false;
+
+  form!: FormGroup;
+
+
   ngOnInit(): void {
+    this.createForm();
     this.getTasks();
+  }
+
+  createForm(): void {
+    this.form = new FormGroup({
+      descricao: new FormControl({value: '', disabled: false}, [Validators.required, Validators.maxLength(30)]),
+    })
+  }
+
+  submit() {
+    this.submitted = true;
+
+    if(this.form.invalid) return;
+
+    const { descricao } = this.form.getRawValue();
+
+    const task: Task = {
+      description: descricao,
+      completed: false,
+    }
+
+    this.taskService.salvar(task).subscribe({
+      next: (task: Task) => {
+        this.getTasks();
+      }
+    })
   }
 
   getTasks() {
     this.taskService.getTasks().subscribe({
       next: (tasks: Task[]) => {
-        this.tasks = tasks;
+        this.tasks = tasks.sort((a, b) => a.id! - b.id!);
         this.isLoadingTasks = false;
       },
       error: (err) => {
@@ -57,5 +99,9 @@ export class HomeComponent implements OnInit {
       },
       error: () => {}
     });
+  }
+
+  get descricao() {
+    return this.form.get('descricao')!;
   }
 }
