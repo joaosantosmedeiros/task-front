d652e82cce95f53f26a51731823c3325b5dd9903
joao pedro santos <jopesame@gmail.com>
2025-01-26 14:36:01 -0300

    add task service

---
+++ src/app/pages/home/service/task.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { first, Observable, retry } from 'rxjs';
+import { Task } from '../../../shared/models/Task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private readonly apiUrl = 'http://localhost:8081/taskback/task';
+  private readonly uriListarTasks = this.apiUrl + '';
+  private readonly uriRemoverTask = this.apiUrl + '';
+  private readonly uriTaskById = this.apiUrl + '';
+  private readonly uriEditarTask = this.apiUrl + '';
+  private readonly uriInserirTask = this.apiUrl + '';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.uriListarTasks).pipe(first(), retry(1));
+  }
+
+  remover(id: number): Observable<any> {
+    return this.http.delete<any>(this.uriRemoverTask + "/" + id);
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this.http.get<Task>(this.uriTaskById + "/" + id)
+      .pipe(first(), retry(1));
+  }
+
+  atualizar(id: number, task: Task): Observable<Task> {
+    return this.http.put<Task>(this.uriEditarTask + "/" + id, task);
+  }
+
+  salvar(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.uriInserirTask, task);
+  }
+}
