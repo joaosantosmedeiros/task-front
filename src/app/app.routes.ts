d275dca91fdfacbb404970c696c5fc76fa0033eb
joao pedro santos <jopesame@gmail.com>
2025-01-26 13:29:24 -0300

    remocao de rotas de login e registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', component: HomeComponent },
-];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
