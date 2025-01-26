3888f0c878ad295a8093d59e5dc21604d6d2ce82
joao pedro santos <jopesame@gmail.com>
2025-01-26 14:36:14 -0300

    add listagem de tasks na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './service/task.service';
+import { Task } from '../../shared/models/Task';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule, MatIconModule, MatButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private taskService: TaskService) {}
 
+  tasks: Task[] = [];
+  isLoadingTasks: boolean = true;
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.taskService.getTasks().subscribe({
+      next: (tasks: Task[]) => {
+        this.tasks = tasks;
+        this.isLoadingTasks = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoadingTasks = false;
+      },
+    });
+  }
+
+  updateStatus(id: number) {
+    this.taskService.getTask(id).subscribe({
+      next: (task: Task) => {
+        task.completed = !task.completed;
+        this.taskService.atualizar(id, task).subscribe({
+          next: () => {
+            this.getTasks();
+          },
+          error: (err) => {}
+        });
+      }
+    })
+
+  }
+
+  deleteTask(id: number) {
+    this.taskService.remover(id).subscribe({
+      next: () => {
+        this.getTasks();
+      },
+      error: () => {}
+    });
+  }
 }
